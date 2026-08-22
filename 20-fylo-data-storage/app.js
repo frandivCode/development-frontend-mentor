28bfd77ab492d52e360a395f10ce57589fc2cb44
frandivCode <fran_zapata.13@live.com>
2026-08-22 16:40:56 -0300

    fix js variables

--- 20-fylo-data-storage/app.js
+++ 20-fylo-data-storage/app.js
@@ -5,12 +5,12 @@ const progressBar = document.getElementById('progress-bar');
 
 
 inputStorage.addEventListener('input', () => {
-    let valorInput = inputStorage.value;
+    const valorInput = inputStorage.value;
     textStorage.textContent = valorInput;
 
-    let gbDisponibles = 1000 - valorInput;
+    const gbDisponibles = 1000 - valorInput;
     tooltipNumber.textContent = gbDisponibles;
 
-    let widthProgress = valorInput / 10;
+    const widthProgress = valorInput / 10;
     progressBar.style.width = widthProgress + '%';
 });
